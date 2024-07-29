8dba6880a634cb494fae8b99c34ef202a22f020a
mukesh1996-ds <mks.mukesh1996@gmail.com>
2024-07-29 10:19:35 +0530

    setup completed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Mukesh",
+    author_email="mks.mukesh1996@gmail.com",
+    packages=find_packages()
+)
